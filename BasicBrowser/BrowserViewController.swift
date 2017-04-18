06c5aa5d443651dab19ed1c30e60bd2b2c5b4068
Wayne <waynehartman@gmail.com>
2017-04-18 09:32:04 -0500

    initial setup

---
+++ BasicBrowser/BrowserViewController.swift
@@ -0,0 +1,25 @@
+//
+//  BrowserViewController.swift
+//  BasicBrowser
+//
+//  Created by Wayne Hartman on 4/18/17.
+//  Copyright © 2017 wh. All rights reserved.
+//
+
+import UIKit
+
+class BrowserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
